e0d3fc0f33b70d1a94270b5872604dc085cdc5f6
Francois SIDLER <fsidler@e2r11p21.42.fr>
2015-12-02 19:06:18 +0100

    fichier.c

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: fsidler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 17:15:27 by fsidler           #+#    #+#             */
-/*   Updated: 2015/12/01 13:56:36 by fsidler          ###   ########.fr       */
+/*   Updated: 2015/12/02 19:04:42 by fsidler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 char	*ft_strncpy(char *dest, const char *src, size_t n)
 {
-	size_t i;
+	size_t	i;
 
-	i = 0;
-	while (src[i] && i < n)
+	if (dest && src)
 	{
-		dest[i] = src[i];
-		i++;
+		ft_memset(dest, '\0', n);
+		i = 0;
+		while (n-- && src[i])
+		{
+			dest[i] = src[i];
+			i++;
+		}
 	}
-	dest[i] = '\0';
 	return (dest);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: fsidler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 15:54:40 by fsidler           #+#    #+#             */
-/*   Updated: 2015/12/02 18:10:07 by fsidler          ###   ########.fr       */
+/*   Updated: 2015/12/02 18:18:43 by fsidler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 char	*ft_strrchr(const char *str, int c)
 {
-	long long	i;
+	size_t	i;
 
 	i = 0;
 	while (str[i])
 		i++;
-	while (i >= 0)
+	while (i + 1 > 0)
 	{
 		if (str[i] == c)
 			return ((char *)(str + i));
